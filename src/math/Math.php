970d013b36af11095b94600175eb33ce5742fe28
KhangTA <truongankhang@gmail.com>
2021-12-31 21:55:43 +0700

    Add code

--- src/math/Math.php
+++ src/math/Math.php
@@ -4,6 +4,24 @@ namespace Acme\Math;
 
 class Math
 {
+    public function sum($num1, $num2) {
+        return $num1 + $num2;
+    }
     
+    public function subtract($num1, $num2) {
+        return $num1 - $num2;
+    }
+    
+    public function multiply($num1, $num2) {
+        return $num1 * $num2;
+    }
+    
+    public function divide($num1, $num2) {
+        if ($num2 == 0) {
+            throw new Exception('Divide by zero');
+        } else {
+            return $num1 / $num2;
+        }
+    }
 }
 
